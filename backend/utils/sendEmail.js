f5618f07e59c50365704efb67055253f2068925e
Your Name <you@example.com>
2026-03-23 15:21:41 +0530

    commit

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,6 +5,7 @@ export const sendEmail = async ({ email, subject, message }) => {
     host: process.env.SMTP_HOST,
     service: process.env.SMTP_SERVICE,
     port: process.env.SMTP_PORT,
+    secure: true, // REQUIRED for port 465 to prevent the connection from hanging forever!
     auth: {
       user: process.env.SMTP_MAIL,
       pass: process.env.SMTP_PASSWORD,
